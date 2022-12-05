0e4f8032716a62645a325134478f1252342102d7
git@github.com:GamesDoneWr0ng/code.git <askborgen@Asks-MacBook-Air-2.local>
2022-12-05 10:28:43 +0100

    "a"

--- python/temp.py
+++ python/temp.py
@@ -3,13 +3,12 @@ from pylab import *
 x = [-3, -2, -1, 0, 1, 2, 3, 4]
 f = [9, 4, 1, 0, 1, 4, 9, 16]
 
-n =len(x)
-g = zeros(n)
+g = zeros(len(x))
 
-for i in range(0, n-1):
-  g[i] = (f[i+1] - f[i]) / (x[i+1] - x[i])
+for i in range(len(x)-1):
+    g[i] = (f[i+1] - f[i]) / (x[i+1] - x[i])
 
-plot(x[0 : n-1], f[0 : n-1])
-plot(x[0 : n-1], g[0 : n-1])
+plot(x[:-1], f[:-1])
+plot(x[:-1], g[:-1])
 grid()
 show()
