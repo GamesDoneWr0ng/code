2a835ad3c3463732bc800c1cd8cfa2d09a3ef233
git@github.com:GamesDoneWr0ng/code.git <askborgen@Asks-MacBook-Air-2.local>
2022-12-05 10:17:43 +0100

    My first commit

--- python/temp.py
+++ python/temp.py
@@ -1,11 +1,15 @@
-from math import e
+from pylab import *
 
-def f(x):
-    return x**2 * e**x
+x = [-3, -2, -1, 0, 1, 2, 3, 4]
+f = [9, 4, 1, 0, 1, 4, 9, 16]
 
-x_1 = 1
-x_2 = 1.00000000000001
+n =len(x)
+g = zeros(n)
 
-stigTall = (f(x_2) - f(x_1)) / (x_2 - x_1)
+for i in range(0, n-1):
+  g[i] = (f[i+1] - f[i]) / (x[i+1] - x[i])
 
-print(stigTall)
+plot(x[0 : n-1], f[0 : n-1])
+plot(x[0 : n-1], g[0 : n-1])
+grid()
+show()
