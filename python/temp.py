7b9c683b9cc32024616a62b313add19cca9580d5
GamesDoneWr0ng <ask@flaten.no>
2023-05-16 16:41:59 +0200

    litt primtall

--- python/temp.py
+++ python/temp.py
@@ -1,7 +1,17 @@
-import pygame as pg
-pg.init()
+import numpy as np
+import cProfile
+import timeit
 
-screen = pg.display.set_mode((600, 600))
+def primes(n):
+    """returns a list of all primes up til n as fast as posible using numpy and numba for max speed"""
+    primes = np.ones(n+1, dtype=np.bool)
+    primes[0] = primes[1] = False
 
-while True:
-    pg.display.flip()
+    for i in range(2, int(np.sqrt(n))+1):
+        if primes[i]:
+            primes[i*i:n+1:i] = False
+    return np.where(primes)[0]
+
+#print(timeit.timeit("primes(10_000_000)", number=10, globals=globals()))
+cProfile.run("primes(10_000_000)")
+#print(primes(10_000_000))
