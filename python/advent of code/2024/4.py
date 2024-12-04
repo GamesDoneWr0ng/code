867fc2e68c2f0e5e85620a290c6b03ca9d634e16
GamesDoneWr0ng <ask@flaten.no>
2024-12-04 08:46:20 +0100

    4-2

--- python/advent of code/2024/4.py
+++ python/advent of code/2024/4.py
@@ -144,7 +144,7 @@ XMAS = np.array([i for i in "XMAS"])
 arr = np.array([[letter for letter in row] for row in data.split("\n")])
 
 count = 0
-y,x = np.where(arr == "X")
+"""y,x = np.where(arr == "X")
 for y,x in zip(y,x):
     for slice in [arr[y,x:x+4], # horizontal right
                   arr[y:y+4,x], # vertical down
@@ -159,6 +159,19 @@ for y,x in zip(y,x):
         if slice.size != 4:
             continue
         if np.all(slice == XMAS):
-            count += 1
+            count += 1"""
 
+y,x = np.where(arr=="A")
+for y,x in zip(y,x):
+    slice = arr[y-1 if y-1>=0 else None:y+2, x-1 if x-1>=0 else None:x+2]
+    if slice.size != 9:
+        continue
+    if slice[0,0] in {"M", "S"} and \
+       slice[0,2] in {"M", "S"} and \
+       slice[2,0] in {"M", "S"} and \
+       slice[2,2] in {"M", "S"} and \
+       slice[0,0] != slice[2,2] and \
+       slice[0,2] != slice[2,0]:
+        count += 1
+        
 print(count)
