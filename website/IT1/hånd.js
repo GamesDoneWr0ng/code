887aa82651795d0c5058ab1daad2bf782f2c104d
GamesDoneWr0ng <ask@flaten.no>
2025-01-08 09:58:20 +0100

    it

---
+++ website/IT1/hånd.js
@@ -0,0 +1,56 @@
+// var a = 200;
+// var b = 4;
+// var c = 5;
+// if (a<b) {
+//     a=b;
+// }
+// if (a<c) {
+//     a=c;
+// }
+// console.log(a);
+
+// var num = 7;
+// console.log((num%2)===0);
+
+// var år = 2025;
+// if ((år%4)===0) {
+//     console.log(år, "er et skuddår");
+// } else {
+//     console.log(år, "er ikke et skuddår");
+// }
+
+
+// var i = 0;
+// while (i<=100) {
+//     console.log(i);
+//     i++;
+// }
+
+// var i = 0;
+// while (true) {
+//     console.log(i);
+//     i += 8;
+// }
+
+// var i = 0;
+// while (i<100) {
+//     if ((i%10)!==0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// for (let i = 0; i<= 100; i++) {
+//     console.log(i);
+// }
+
+// for (let i=0; i<100; i++) {
+//     if((i%10) !== 0) {
+//         console.log(i);
+//     }
+// }
+
+for (let i=0; true; i+=8) {
+    console.log(i);
+}
