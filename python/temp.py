e50717b46934133b6c3f89df350e120e0bae2625
GamesDoneWr0ng <ask@flaten.no>
2025-06-02 12:44:54 +0200

    Microbit

--- python/temp.py
+++ python/temp.py
@@ -1,21 +1,15 @@
 import matplotlib.pylab as plt
 
-with open("/Users/askborgen/Downloads/microbit.csv") as f:
+with open('/Users/askborgen/Downloads/microbit (1).csv') as f:
     data = f.readlines()
 
 t_values = []
-x_values = []
-y_values = []
-z_values = []
+reading_values = []
 
 for i in data[1:]:
-    t,x,y,z = i.split(",")
+    t,reading = i.split(",")
     t_values.append(float(t))
-    x_values.append(float(x))
-    y_values.append(float(y))
-    z_values.append(float(z))
+    reading_values.append(float(reading))
 
-plt.plot(t_values, x_values, "r-")
-plt.plot(t_values, y_values, "b-")
-plt.plot(t_values, z_values, "g-")
+plt.plot(t_values, reading_values, "r-")
 plt.show()
